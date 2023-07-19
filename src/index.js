0d58ca0a52d93fd78014d7770f31c696c98f0b25
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-07-19 08:22:56 -0300

    fix: use index.js

--- src/index.js
+++ src/index.js
@@ -8,5 +8,5 @@ import App from './App'
 
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('app');
-const root = createRoot(container!);
+const root = createRoot(container);
 root.render(<App />);
